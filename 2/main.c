0a5e1b76aadd907da496c8efa6fa015755d16b6d
b05502087 <b05502087@linux1.csie.ntu.edu.tw>
2018-03-14 22:18:32 +0800

    problem 2 v1

---
+++ 2/main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#define max 10010
+#define DATA char
+//#define debug_input
+//#define debug_process
+
+struct ArrayStack{
+    int top;
+    int capacity;
+    DATA*array;
+};
+
+struct ArrayStack* CreateStack(){
+    struct ArrayStack*S=malloc(sizeof(struct ArrayStack));
+    if(!S)return NULL;
+    S->capacity=max;
+    S->top=-1;
+    S->array=(DATA *)malloc(S->capacity*sizeof(DATA));
+    if(!S->array)return NULL;
+    return S;
+};
+
+int IsEmptyStack(struct ArrayStack *S){
+    return (S->top==-1);
+}
+
+int IsFullStack(struct ArrayStack *S){
+    return (S->top==S->capacity-1);
+}
+
+void Push(struct ArrayStack*S,DATA data){
+    if(IsFullStack(S))
+        printf("Stack Overflow");
+    else
+        S->array[++S->top]=data;
+}
+
+DATA Pop(struct ArrayStack*S){
+    if(IsEmptyStack(S)){
+        printf("Stack is Empty");
+        return 0;
+    }
+    else
+        return (S->array[S->top--]);
+}
+
+int char_to_int(char c){
+    return (c-'0');
+}
+
+int main()
+{
+    char tmp;
+    char array[max];
+    int remove;
+    scanf("%s %d",array,&remove);
+    int total=strlen(array);
+    #ifdef debug_input
+        printf("%s\n%d\n%d\n",array,remove,total);
+    #endif // debug_input
+
+    //造一個stack
+    struct ArrayStack* ptrStack=CreateStack();
+    assert(ptrStack!=NULL);
+
+
+    int i;
+    int remove_times=remove;
+    for(i=0;array[i]!='\0';i++){
+        #ifdef debug_process
+            printf("char is %c\n",array[i]);
+        #endif // debug_process
+        #ifdef debug_process
+            printf("empty==%d\n",IsEmptyStack(ptrStack));
+        #endif // debug_process
+        if(!IsEmptyStack(ptrStack)){
+            #ifdef debug_process
+                printf("remove_times==%d\n",remove_times);
+            #endif // debug_process
+            if(remove_times>0){
+                #ifdef debug_process
+                    printf("char==%d,top==%d\n",char_to_int(array[i]),char_to_int(ptrStack->array[ptrStack->top]));
+                    if(char_to_int(array[i])<char_to_int(ptrStack->array[ptrStack->top])){
+                        printf("Pop!\n");
+                    }
+                    else{
+                        printf("No Pop!\n");
+                    }
+                #endif // debug_process
+                if(char_to_int(array[i])<char_to_int(ptrStack->array[ptrStack->top])){
+                    Pop(ptrStack);
+                    remove_times--;
+                }
+            }
+        }
+        Push(ptrStack,array[i]);
+    }
+
+    //Q:已經掃完一遍，但是remove_times卻還有剩?
+    //A:取最右邊的total-remove個字就好(因為他們整個一定是遞增的)
+    for(i=0;i<total-remove;i++){
+        printf("%c",ptrStack->array[i]);
+    }
+
+    return 0;
+}
+
