7e3a3902ff222c97a0c740de51815a371e28422d
b05502087 <b05502087@linux1.csie.ntu.edu.tw>
2018-06-09 21:30:20 +0800

    problem2_no_graph

--- 11/main.c
+++ 11/main.c
@@ -1,44 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define max_events 52
-
-//#define debug_input
-//#define debug_graph
-#define max_vertex 524288//(2^19)
-
+#define max_vertex 600000
 #define WHITE 0
-#define BLACK 2
 #define GRAY 1
-#define infinte 2147483647
-#define NIL -1
-struct node_attribute{
-    int color;
-    int distance;
-    //int parent;
-};
-typedef struct node_attribute node_attribute;
-
-struct adjacency_node{
-    int number;
-    struct adjacency_node*next;
-};
-typedef struct adjacency_node adjacency_node;
-
-struct adjacency_list{
-    struct adjacency_node*head;
-};
-typedef struct adjacency_list adjacency_list;
-
-struct Graph{
-    int num_of_vertex;
-    struct adjacency_list*list_array;
-};
-typedef struct Graph Graph;
-
-void print_graph(Graph*graph);
-struct Graph* create_graph(int num_vertex);
-void add_edge(Graph*graph,int start,int end);
-struct adjacency_node*add_node(int num);
+#define BLACK 2
 
 struct Queue{
     int front,rear,size;
@@ -53,32 +20,41 @@ int dequeue(Queue*queue);
 int isFull(Queue*queue);
 int isEmpty(Queue*queue);
 
-void BFS(Graph*graph,int start);
 
-int events_array[max_events];
-int events_times[max_events];
-node_attribute attribute_array[max_vertex];
-
+int Events[max_events];
+int distance[max_vertex];
+int color[max_vertex];
 int main()
 {
-    Graph*graph=create_graph(max_vertex);
-    /*0~524287*/
     int N_events;
     scanf("%d",&N_events);
-    int i,j,XOR_result,Events,Events_times;
+    int i,Events_times;
     for(i=0;i<N_events;i++){
         //scanf("%d %d",&events_array[i],&events_times[i]);
-        scanf("%d %d",&Events,&Events_times);
+        scanf("%d %d",&Events[i],&Events_times);
         #ifdef debug_input
             printf("%d %d\n",Events,Events_times);
         #endif // debug_input
-        for(j=0;j<max_vertex;j++){
-            XOR_result=j^Events;
-            /*在j和XOR_result之間建立一條邊*/
-            add_edge(graph,j,XOR_result);
-        }
     }
-    BFS(graph,0);
+
+    Queue *queue=create_queue(max_vertex);
+    distance[0]=0;
+    color[0]=GRAY;
+    enqueue(queue,0);
+    int u,v;
+    while(!isEmpty(queue)){
+        u=dequeue(queue);
+        for(i=0;i<N_events;i++){
+            v=u^Events[i];
+            if(color[v]==WHITE){
+                distance[v]=distance[u]+1;
+                color[v]=GRAY;
+                enqueue(queue,v);
+            }
+        }
+        color[u]=BLACK;
+    }
+
     int Q_s_t;
     scanf("%d",&Q_s_t);
     int s,t,goal;
@@ -89,129 +65,20 @@ int main()
         #endif // debug_input
         goal=s^t;
         /*藉由BFS已知0到goal的最短距離*/
-        if(attribute_array[goal].distance==0){
+        if(distance[goal]==0){
             if(goal==0){
-                printf("%d\n",attribute_array[goal].distance);
+                printf("%d\n",distance[goal]);
             }
             else{
                 printf("UNREACHABLE\n");
             }
         }
         else{
-            printf("%d\n",attribute_array[goal].distance);
+            printf("%d\n",distance[goal]);
         }
     }
-
-    #ifdef debug_input
-        printf("N_events==%d\n",N_events);
-        for(i=0;i<N_events;i++){
-            //printf("events==%d events_times==%d\n",events_array[i],events_times[i]);
-
-        }
-    #endif // debug_input
-    #ifdef debug_graph
-        print_graph(graph);
-    #endif // debug_graph
     return 0;
 }
-int main01(){/*test graph ds*/
-    Graph*graph=NULL;
-    graph=create_graph(10);
-    /*edge只有0~9*/
-    add_edge(graph,0,1);
-    add_edge(graph,2,5);
-    add_edge(graph,0,9);
-    add_edge(graph,1,6);
-    //print_graph(graph);
-    BFS(graph,0);
-    int i;
-    for(i=0;i<10;i++){
-        printf("%d %d %d\n",attribute_array[i].color\
-                            ,attribute_array[i].distance\
-                            /*,attribute_array[i].parent*/);
-    }
-}
-int main02(){//test queue ds
-    Queue*queue=create_queue(10);
-    enqueue(queue,3);
-    enqueue(queue,2);
-    enqueue(queue,10);
-    dequeue(queue);
-    dequeue(queue);
-    printf("%d\n",queue->array[queue->front]);
-}
-void BFS(Graph*graph,int start){
-    int i;
-    /*
-    for(i=0;i<graph->num_of_vertex;i++){
-        attribute_array[i].color=WHITE;
-        attribute_array[i].distance=infinte;
-        attribute_array[i].parent=NIL;
-    }
-    */
-    attribute_array[start].color=GRAY;
-    //attribute_array[start].parent=NIL;
-    attribute_array[start].distance=0;
-
-    Queue*queue=create_queue(max_vertex);
-    enqueue(queue,start);
-    int u,v;
-    adjacency_node*ptr;
-    while(!isEmpty(queue)){
-        u=dequeue(queue);
-        ptr=graph->list_array[u].head;
-        while(ptr!=NULL){
-            v=ptr->number;
-            if(attribute_array[v].color==WHITE){
-                attribute_array[v].color=GRAY;
-                attribute_array[v].distance=\
-                    attribute_array[u].distance+1;
-                //attribute_array[v].parent=u;
-                enqueue(queue,v);
-            }
-            ptr=ptr->next;
-        }
-        attribute_array[u].color=BLACK;
-    }
-}
-void print_graph(Graph*graph){
-    adjacency_node*ptr;
-    int i;
-    for(i=0;i<graph->num_of_vertex;i++){
-        printf("adj_list of %d :",i);
-        ptr=graph->list_array[i].head;
-        while(ptr!=NULL){
-            printf("->%d",ptr->number);
-            ptr=ptr->next;
-        }
-        printf("\n");
-    }
-    return;
-}
-struct Graph* create_graph(int num_vertex){
-     struct Graph* graph=(struct Graph*)\
-        malloc(sizeof(struct Graph));
-     graph->num_of_vertex=num_vertex;
-     graph->list_array=(adjacency_list*)\
-        malloc(num_vertex*sizeof(adjacency_list));
-     int i;
-     for(i=0;i<num_vertex;i++){
-        graph->list_array[i].head=NULL;
-     }
-     return graph;
-}
-void add_edge(Graph*graph,int start,int end){
-    struct adjacency_node*new_node=add_node(end);
-    new_node->next=graph->list_array[start].head;
-    graph->list_array[start].head=new_node;
-}
-struct adjacency_node*add_node(int num){
-    adjacency_node* ptr_new_node=\
-        (adjacency_node*)malloc(sizeof(adjacency_node));
-    ptr_new_node->next=NULL;
-    ptr_new_node->number=num;
-    return ptr_new_node;
-};
 
 Queue*create_queue(unsigned int create_capacity){
     Queue*queue=(Queue*)malloc(sizeof(Queue));
@@ -248,4 +115,13 @@ int isFull(Queue*queue){
 int isEmpty(Queue*queue){
     return (queue->size==0);
 }
+/*
+2
+5 2
+4 1
+3
+0 5
+1 0
+1 2
+*/
 
